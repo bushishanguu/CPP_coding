a5d48d52ba6486db7c599d9e8de8544452fa79fa
bushishanguu <1750169605@qq.com>
2019-11-05 23:23:00 +0800

    给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。

---
+++ 搜索插入位置（力扣）/搜索插入位置（力扣）/test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+	int searchInsert(vector<int>& nums, int target) {
+
+		// 若找到则返回当前的位置
+		// 若找不到则返回left指针的位置
+		int left = 0, right = nums.size() - 1, mid;
+		while (left <= right)
+		{
+			mid = (left + right) / 2;
+			if (nums[mid] > target) right = mid - 1;
+			else if (nums[mid] < target) left = mid + 1;
+			else return mid;
+		}
+		return left;
+	}
+};
