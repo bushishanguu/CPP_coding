7fc5686c58d5808b1c3662dba66bf88a6c4450cb
bushishanguu <1750169605@qq.com>
2019-11-26 20:43:35 +0800

    list的模拟实现 增删

--- list的模拟实现/list的模拟实现/test.cpp
+++ list的模拟实现/list的模拟实现/test.cpp
@@ -18,7 +18,7 @@ struct Node
 
 	//构造函数
 	Node(const T& val = T()) :
-		_prev(nullptr), _next(nullptr), val(val)
+		_prev(nullptr), _next(nullptr), _val(val)
 	{}
 };
 
@@ -43,7 +43,7 @@ struct Iterator{
 	}
 	//!=的重载
 	bool operator!=(const Iterator& it){
-		return it.node != this->node;
+		return it._node != this->_node;
 	}
 
 	//->的重载
@@ -79,6 +79,7 @@ struct Const_Iterator{
 	bool operator!=(const Const_Iterator& cit){
 		return _node != cit._node;
 	}
+};
 
 //List类
 template <class T>
@@ -90,8 +91,8 @@ public:
 	//构造函数，建立一个不存在数据元素的结点—头节点
 	List()
 		:_head(new Node){
-		_head->_next = head;
-		_head->_prev = head;
+		_head->_next = _head;
+		_head->_prev = _head;
 	}
 
 	//尾插
@@ -113,23 +114,125 @@ public:
 			_head->_prev = node;
 		}
 	}
+	//头插
+	void PushFront(const T& val){
+		Node* node = new Node;
+		node->_val = val;
+		//如果原链表没有有效元素
+		if (_head->_next == _head){
+			node->_next = _head;
+			_head->_next = node;
+			node->_prev = _head;
+			_head->_prev = node;
+		}
+		//原链表中存在有效元素
+		else{
+			node->_next = _head->_next;
+			_head->_next->_prev = node;
+			_head->_next = node;
+			node->_prev = _head;
+		}
+	}
+
+	//尾删
+	void PopBack(){
+		//如果没有有效元素
+		if (_head->_next == _head){
+			return;
+		}
+		//存在有效元素
+		Node* tmp = _head->_prev;
+		_head->_prev = tmp->_prev;
+		tmp->_prev->_next = _head;
+		delete tmp;
+	}
+
+	//头删
+	void PopFront(){
+		//如果没有有效元素
+		if (_head->_next == _head){
+			return;
+		}
+		//存在有效元素
+		Node* tmp = head->_next;
+		_head->next = tmp->_next;
+		tmp->_next->_prev = _head;
+		delete tmp;
+	}
+
+	iterator begin(){
+		return iterator(_head->_next);
+	}
+
+	iterator end(){
+		return iterator(_head);
+	}
+
+	const_iterator cbegin(){
+		return const_iterator(_head->_next);
+	}
+
+	const_iterator cend(){
+		return const_iterator(_head);
+	}
 
 
+	iterator Erase(iterator pos){
+		//判断链表是否为空
+		if (pos != end()){
+			iterator tmp = pos;
+			tmp._node->_prev->_next = tmp._node->_next;
+			tmp._node->_next->_prev = tmp._node->_prev;
+			++pos;
+			delete tmp._node;
+			tmp._node = nullptr;
+		}
+		return pos;
+	}
 
-	};
-
-
-
-
-
+	//打印
+	void Print(){
+		iterator it = begin();
+		while (it != end()){
+			cout << *it << " ";
+			++it;
+		}
+		cout << endl;
+	}
+private:
+	Node* _head;
 };
 
+
+void test(){
+	List<int> l;
+	/*l.PushBack(1);
+	l.PushBack(2);*/
+	l.PushFront(1);
+	l.PushFront(2);
+	//l.PopBack();
+	//l.PopBack();
+	//l.PopFront();
+	//l.PopFront();
+
+	List<int>::iterator it = l.begin();
+	*it = 100;
+	cout << it._node->_val << endl;
+
+
+	List<int>::iterator it2 = l.Erase(it);
+	cout << it2._node->_val << endl;
+	l.Print();
+	List<int>::const_iterator cit = l.cbegin();
+
+}
 
 
 
 
 int main()
 {
+	test();
 	system("pause");
 	return 0;
 }
