f4d7658b743a450c67bf4fe384d32c2628b5835a
bushishanguu <1750169605@qq.com>
2020-04-01 15:59:33 +0800

    变态跳台阶

---
+++ 变态跳台阶/变态跳台阶/test.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+	int jumpFloorII(int number) {
+		if (number == 0 || number == 1)
+		{
+			return 1;
+		}
+		if (number == 2)
+		{
+			return 2;
+		}
+		int sum = 0;
+		for (int i = 0; i<number; i++)
+		{
+			sum += jumpFloorII(i);
+		}
+		return sum;
+
+	}
+};
