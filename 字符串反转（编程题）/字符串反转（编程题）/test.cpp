b9d3cb3d11cdb0ffa0d4c2b7cf11e283607e9d9c
bushishanguu <1750169605@qq.com>
2020-04-12 20:05:14 +0800

    字符串反转

---
+++ 字符串反转（编程题）/字符串反转（编程题）/test.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<string> 
+#include<iostream> 
+using namespace std;
+
+string reverseString(string s) 
+{ 
+	if (s.empty())       
+		return s;    
+	size_t start = 0;   
+	size_t end = s.size() - 1;    
+	while (start < end)    
+	{ 
+		swap(s[start], s[end]);       
+		++start;        
+		--end; 
+	}    
+	return s; 
+}
+
+int main() 
+{ 
+	string s; 
+	getline(cin, s); 
+	cout << reverseString(s) << endl; 
+	return 0; 
+}
